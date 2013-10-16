c913cbd57aef87773238cde86e86b82a30c5a543
greenmachine1 <scary_penguins05@hotmail.com>
2013-10-16 01:08:07 -0700

    working on implementing my cursor to go through my json info

--- Week_3/src/com/Cory/week_3/CollectionProvider.java
+++ Week_3/src/com/Cory/week_3/CollectionProvider.java
@@ -1,28 +1,76 @@
 package com.Cory.week_3;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.MatrixCursor;
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public class CollectionProvider extends ContentProvider{
+	
+	public static final String FILE_NAME = "json_info.txt";
+	
+	public static final String AUTHORITY = "com.Cory.week_3.collectionprovider";
+	
+	public static class WeatherData implements BaseColumns {
+		
+		public static final Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/items");
+		
+		public static final String CONTENT_TYPE = "vnd.android.cursor.dir/vnd.Cory.week_3.item";
+		public static final String CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd.Cory.week_3.item";
+		
+		//Define Columns
+		public static final String PRESSURE_COLUMN = "pressure";
+		public static final String WEATHER_COLUMN = "weather";
+		public static final String SPEED_COLUMN = "speed";
+		
+		public static final String[] PROJECTION= {"_Id", PRESSURE_COLUMN, WEATHER_COLUMN, SPEED_COLUMN};
+		
+		/* constructor */ 
+		private WeatherData(){};
+		
+		
+	}
+	
+	public static final int ITEMS = 1;
+	public static final int ITEMS_ID = 2;
+	
+	private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
+	static {
+		uriMatcher.addURI(AUTHORITY, "items/", ITEMS);
+		uriMatcher.addURI(AUTHORITY, "items/", ITEMS_ID);
+	}
+	
 	@Override
-	public int delete(Uri arg0, String arg1, String[] arg2) {
+	public int delete(Uri uri, String selection, String[] selectionArgs) {
 		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
-	public String getType(Uri arg0) {
+	public String getType(Uri uri) {
 		// TODO Auto-generated method stub
+		switch (uriMatcher.match(uri)){
+		case ITEMS:
+			return WeatherData.CONTENT_TYPE;
+			
+		case ITEMS_ID:
+			return WeatherData.CONTENT_ITEM_TYPE;
+		}
+		
 		return null;
 	}
 
 	@Override
 	public Uri insert(Uri arg0, ContentValues arg1) {
 		// TODO Auto-generated method stub
-		return null;
+		throw new UnsupportedOperationException();
 	}
 
 	@Override
@@ -32,8 +80,30 @@ public class CollectionProvider extends ContentProvider{
 	}
 
 	@Override
-	public Cursor query(Uri arg0, String[] arg1, String arg2, String[] arg3,
-			String arg4) {
+	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
+			String sortOrder) {
+		
+		MatrixCursor result = new MatrixCursor(WeatherData.PROJECTION);
+		
+		String JSONString = FileManager.getInstance().readStringFile(getContext(), FILE_NAME);
+		
+		/* setting up my JSON objects and arrays */
+		JSONObject job = null;
+    	JSONObject city = null;
+    	JSONArray results = null;
+    	JSONArray weather = null;
+		
+		
+		
+		
+		switch (uriMatcher.match(uri)){
+		case ITEMS:
+//			return WeatherData.CONTENT_TYPE;
+			
+		case ITEMS_ID:
+//			return WeatherData.CONTENT_ITEM_TYPE;
+		}
+
 		// TODO Auto-generated method stub
 		return null;
 	}
@@ -41,7 +111,7 @@ public class CollectionProvider extends ContentProvider{
 	@Override
 	public int update(Uri arg0, ContentValues arg1, String arg2, String[] arg3) {
 		// TODO Auto-generated method stub
-		return 0;
+		throw new UnsupportedOperationException();
 	}
 
 }
