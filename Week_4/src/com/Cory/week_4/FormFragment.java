cf3fb5a76b2cba67181a162d74d8a2114ae98242
greenmachine1 <scary_penguins05@hotmail.com>
2013-10-26 12:38:43 -0700

    creation of my fragment

---
+++ Week_4/src/com/Cory/week_4/FormFragment.java
@@ -0,0 +1,7 @@
+package com.Cory.week_4;
+
+import android.app.Fragment;
+
+public class FormFragment extends Fragment {
+
+}
---
+++ Week_4/src/com/Cory/week_4/MoreInfoFragment.java
@@ -0,0 +1,7 @@
+package com.Cory.week_4;
+
+import android.support.v4.app.Fragment;
+
+public class MoreInfoFragment extends Fragment {
+
+}
